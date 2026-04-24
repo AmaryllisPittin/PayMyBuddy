16947d1ef4f815bd496b9ad6f7dd088b8ebf2a2d
Amaryllis Pittin <amaryllispittin@gmail.com>
2026-04-24 17:40:40 +0200

    Utilisation de BCrypt pour protéger les mots de passe

--- pay-my-buddy/pay-my-buddy/src/main/java/com/PayMyBuddy/pay_my_buddy/Service/UserService.java
+++ pay-my-buddy/pay-my-buddy/src/main/java/com/PayMyBuddy/pay_my_buddy/Service/UserService.java
@@ -1,5 +1,6 @@
 package com.PayMyBuddy.pay_my_buddy.Service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.PayMyBuddy.pay_my_buddy.Entity.UserEntity;
@@ -9,21 +10,33 @@ import com.PayMyBuddy.pay_my_buddy.Repository.UserRepository;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void register(UserEntity userEntity) {
         if (userRepository.existsByEmail(userEntity.getEmail())) {
             throw new RuntimeException("Cet email est déjà utilisé.");
         }
+        String hashedPassword = passwordEncoder.encode(userEntity.getPassword());
+        userEntity.setPassword(hashedPassword);
         userRepository.save(userEntity);
     }
 
-    public UserEntity login(String email) {
-        return userRepository.findByEmail(email)
+    public UserEntity login(String email, String password) {
+
+        UserEntity user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new RuntimeException("l'utilisateur est introuvable."));
+
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new RuntimeException("Mot de passe invalide.");
+        }
+
+        return user;
+
     }
 
 }
